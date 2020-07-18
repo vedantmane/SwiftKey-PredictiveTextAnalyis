64d3fd2baf98166293d0db60b5d95a5f0225505a
Vedant Mane <vedant12mane@gmail.com>
2020-07-18 19:49:16 +0530

    N-Grams Plots & WordCloud - EDA

---
+++ plotNGrams.R
@@ -0,0 +1,20 @@
+require(ggplot2)
+plotU <- ggplot(data=dfU, aes(x=word, y=frequency,fill=frequency)) + 
+    geom_bar(stat="identity")+guides(fill=FALSE)+
+    theme(axis.text.x=element_text(angle=90))+
+    scale_x_discrete(limits=dfU$word)+
+    labs(title="Top10 Unigrams")+xlab("words")
+plotB <- ggplot(data=dfB, aes(x=word, y=frequency,fill=frequency)) + 
+    geom_bar(stat="identity")+guides(fill=FALSE)+
+    theme(axis.text.x=element_text(angle=90))+
+    scale_x_discrete(limits=dfB$word)+
+    labs(title="Top10 Bigrams")+xlab("words")
+plotT <- ggplot(data=dfT, aes(x=word, y=frequency,fill=frequency)) + 
+    geom_bar(stat="identity")+guides(fill=FALSE)+
+    theme(axis.text.x=element_text(angle=90))+
+    scale_x_discrete(limits=dfT$word)+
+    labs(title="Top10 Trigrams")+xlab("words")
+plot_grid(plotU, plotB, plotT, nrow = 1, ncol = 3)
+
+set.seed(5)
+wordcloud(names(freqUni), freqUni, max.words = 100, scale = c(3, 0.1), colors = brewer.pal(8,"Dark2"))
